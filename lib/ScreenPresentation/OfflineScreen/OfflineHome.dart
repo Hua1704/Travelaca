db3f64b526749e5ce6acecfcdad7f68aae075391
Buu Pham <pphambuu@gmail.com>
2025-01-03 01:25:12 +0700

    update

--- lib/ScreenPresentation/OfflineScreen/OfflineHome.dart
+++ lib/ScreenPresentation/OfflineScreen/OfflineHome.dart
@@ -1,7 +1,31 @@
+import 'dart:io';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import '../../utils/NetworkMonitor.dart';
-import 'package:travelaca/MainPage.dart'; // Your online page file
+import 'package:travelaca/MainPage.dart';
 
+import '../LoginScreen/LoginScreen.dart'; // Your online page file
+Future<bool> checkConnection() async {
+  try {
+    // Check the connectivity status
+    var connectivityResult = await Connectivity().checkConnectivity();
+
+    if (connectivityResult == ConnectivityResult.none) {
+      // No network detected
+      return false;
+    }
+    // Check actual internet access
+    final result = await InternetAddress.lookup('example.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      return true;
+    }
+  } catch (e) {
+    // Handle exceptions, such as no network or DNS issues
+    print("Error checking internet connection: $e");
+  }
+  return false; // Default to no connection if checks fail
+}
 class OfflineHomeScreen extends StatefulWidget {
   @override
   _OfflineHomeScreenState createState() => _OfflineHomeScreenState();
@@ -11,24 +35,42 @@ class _OfflineHomeScreenState extends State<OfflineHomeScreen> {
   @override
   void initState() {
     super.initState();
-    NetworkManager().startMonitoring(context, MainPage(userId: "", role: ""), OfflineHomeScreen());
+    //NetworkManager().startMonitoring(context, MainPage(userId: "", role: ""), OfflineHomeScreen());
   }
 
   @override
   void dispose() {
-    NetworkManager().stopMonitoring();
+    //NetworkManager().stopMonitoring();
     super.dispose();
   }
-
+  Future<void> _refresh()async {
+    bool hasConnection = await checkConnection();
+    if (hasConnection) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OfflineHomeScreen(), // Replace with OfflineHomepage
+        ),
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Offline Mode'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: ListView(
+          physics: const AlwaysScrollableScrollPhysics(),
           children: [
             Icon(Icons.wifi_off, size: 100, color: Colors.grey),
             SizedBox(height: 20),
